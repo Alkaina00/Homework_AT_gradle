7e01d33e0bfead1110611309888ef3974a0d21a0
Alina Gainulina <alina.gajnullina2015@yandex.ru>
2026-08-02 23:25:14 +0500

    Дз 14
    В рамках выполнения дз14 были реализованы классы:
    Задание 14.1 Кинцо
    MovieRatingComparator - реализует интерфейс: Comparator<Movie>
    метод compare() так, чтобы фильмы сортировались по рейтингу - от меньшего рейтинга к большему.
    
    Задание 14.2 Утилита и тесты
    класс XMLUtils
    createEmptyElement(String tagName) Метод принимает название XML-тега и возвращает строку с открывающим и закрывающим тегом

---
+++ src/main/java/ru/alfa/homework14/Movie.java
@@ -0,0 +1,24 @@
+package ru.alfa.homework14;
+
+public class Movie {
+    private String name;
+    private double rating;
+
+    public Movie(String name, double rating) {
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString(){
+        return "Название: " + name + "\nРейтинг: " + rating;
+    }
+}
---
+++ src/test/java/homework14/XMLUtilsTest.java
@@ -0,0 +1,41 @@
+package homework14;
+
+import org.junit.jupiter.api.Test;
+import ru.alfa.homework14.XMLUtils;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class XMLUtilsTest {
+    @Test
+    public void testCreateEmptyElement_ValidTag() {
+        String tagName = "user";
+        String expected = "<user></user>";
+
+        String result = XMLUtils.createEmptyElement(tagName);
+
+        assertEquals(expected, result,
+                "Ошибка: для тега 'user' ожидался результат '<user></user>', но получен '" + result + "'");
+    }
+
+    @Test
+    public void testCreateEmptyElement_NullTag() {
+        String tagName = null;
+        String expected = "<invalid/>";
+
+        String result = XMLUtils.createEmptyElement(tagName);
+
+        assertEquals(expected, result,
+                "Ошибка: для null ожидался результат '<invalid/>', но получен '" + result + "'");
+    }
+
+    @Test
+    public void testCreateEmptyElement_EmptyString() {
+        String tagName = "";
+        String expected = "<invalid/>";
+
+        String result = XMLUtils.createEmptyElement(tagName);
+
+        assertEquals(expected, result,
+                "Ошибка: для пустой строки ожидался результат '<invalid/>', но получен '" + result + "'");
+    }
+}
